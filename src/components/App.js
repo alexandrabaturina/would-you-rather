0930a4e2b2fae10cf9f71d3e63cc35b72bdf0500
alexandrabaturina <alexandra.v.baturina@gmail.com>
2021-11-02 14:22:21 -0700

    feat: show loading bar

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,13 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import handleInitialData from '../actions/shared';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import LoadingBar from 'react-redux-loading';
+import Home from './Home';
+import NavBar from './NavBar';
+import Question from './Question';
+import NewQuestion from './NewQuestion';
+import Leaderboard from './Leaderboard';
 
 const App = () => {
 
@@ -11,10 +18,24 @@ const App = () => {
     dispatch(handleInitialData())
   }, []);
 
+  const loading = useSelector(state => state.authedUser === null)
+  const authedUser = useSelector(state => state.authedUser)
+
   return (
-    <div>
-      Would You Rather
-    </div>
+    <Router>
+      <NavBar />
+      <LoadingBar />
+      {loading
+        ? null
+        : <>
+          <Route path='/' exact component={Home} />
+          <Route path='/questions/:question_id' component={Question} />
+          <Route path='/add' component={NewQuestion} />
+          <Route path='/leaderboard' component={Leaderboard} />
+        </>
+      }
+
+    </Router>
   )
 };
 
