42ca27754db93e3dde668e680586f05e7926b40c
alexandrabaturina <alexandra.v.baturina@gmail.com>
2021-11-03 15:07:54 -0700

    feat: display questions

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,25 @@
+import { useSelector } from "react-redux";
 
-const Dashboard = ({ title }) => {
+const Dashboard = ({ title, ids }) => {
+
+    //Get data from the store
+    const questions = useSelector(state =>
+
+        ids.map(id => ({
+            id: state.questions[id].id,
+            author: state.questions[id].author,
+            optionOne: state.questions[id].optionOne.text,
+            optionTwo: state.questions[id].optionTwo.text
+        }))
+    )
 
     return (
         <div>
             <h1>{title}</h1>
+            <ul>
+                {questions.map(question =>
+                    <li key={question.id}>Would you rather {question.optionOne} or {question.optionTwo}?</li>)}
+            </ul>
         </div>
     )
 };
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,19 @@
 import { useState } from "react"
+import { useSelector } from "react-redux";
 import { Tabs, Tab, Container } from 'react-bootstrap';
 import Dashboard from './Dashboard';
 
 const Home = () => {
+
+    // Get data from the store
+    const answeredIds = useSelector(state =>
+        Object.keys(state.users[state.authedUser].answers)
+    );
+
+    const unansweredIds = useSelector(state =>
+        Object.keys(state.questions)
+            .filter(id => answeredIds.indexOf(id) === -1)
+    );
 
     const [key, setKey] = useState('unanswered');
 
@@ -13,13 +24,16 @@ const Home = () => {
                 onSelect={k => setKey(k)}>
 
                 <Tab eventKey="unanswered" title="Unanswered">
-                    <Dashboard title={"Unanswered Questions"} />
+                    <Dashboard
+                        title={"Unanswered Questions"}
+                        ids={unansweredIds} />
                 </Tab>
 
                 <Tab eventKey="answered" title="Answered">
-                    <Dashboard title={"Answered Questions"} />
+                    <Dashboard
+                        title={"Answered Questions"}
+                        ids={answeredIds} />
                 </Tab>
-
             </Tabs>
         </Container>
     )
