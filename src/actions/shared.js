3f1470d8206dbc3925da5c9b99c4ba4c14d6dede
alexandrabaturina <alexandra.v.baturina@gmail.com>
2021-11-03 16:13:13 -0700

    fix: default import

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,6 +1,6 @@
 import { getInitialData } from '../utils/api';
 import receiveUsers from './users';
-import receiveQuestions from './questions';
+import { receiveQuestions } from './questions';
 import setAuthedUser from './authedUser';
 import { hideLoading, showLoading } from "react-redux-loading";
 
