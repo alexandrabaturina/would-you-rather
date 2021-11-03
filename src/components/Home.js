a48e1839a366085a4a0199125e6d22789afa7853
alexandrabaturina <alexandra.v.baturina@gmail.com>
2021-11-03 10:46:00 -0700

    feat: display tabs

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,24 +1,27 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useState } from "react"
+import { Tabs, Tab, Container } from 'react-bootstrap';
+import Dashboard from './Dashboard';
 
 const Home = () => {
 
-    const questionsIds = useSelector(state => Object.keys(state.questions))
-    // const authedUser = useSelector(state => state.authedUser)
-    // const users = useSelector(state => state.users[authedUser].answers)
-
-    console.log(questionsIds)
-
+    const [key, setKey] = useState('unanswered');
 
     return (
-        <div>
-            <h1>Home</h1>
-            <div>
-                <h2>All Question IDs</h2>
-                {questionsIds.map(id => (
-                    <p>ID: {id}</p>
-                ))}
-            </div>
-        </div>
+        <Container>
+            <Tabs
+                activeKey={key}
+                onSelect={k => setKey(k)}>
+
+                <Tab eventKey="unanswered" title="Unanswered">
+                    <Dashboard title={"Unanswered Questions"} />
+                </Tab>
+
+                <Tab eventKey="answered" title="Answered">
+                    <Dashboard title={"Answered Questions"} />
+                </Tab>
+
+            </Tabs>
+        </Container>
     )
 };
 
