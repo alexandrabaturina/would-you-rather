ffb849a9893bc9292c05beeb7adfc500d38ed44c
alexandrabaturina <alexandra.v.baturina@gmail.com>
2021-11-02 21:14:06 -0700

    refactor: use defaultValue instead of setting 'selected' on <option>

--- src/components/Login.js
+++ src/components/Login.js
@@ -8,27 +8,28 @@ const Login = () => {
     const users = useSelector(state =>
         Object.values(state.users)
             .map(item => ({ name: item.name, id: item.id }))
-    )
+    );
 
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
 
-    const [selectedUser, setSelectedUser] = useState(null)
+    const [selectedUser, setSelectedUser] = useState(null);
 
     const handleSelect = e => {
         setSelectedUser(e.target.options[e.target.selectedIndex].dataset.id)
-    }
+    };
 
     const handleSubmit = e => {
-        e.preventDefault()
-        dispatch(setAuthedUser(selectedUser))
-    }
+        e.preventDefault();
+        dispatch(setAuthedUser(selectedUser));
+
+    };
 
     return (
         <div>
             <h1>Login</h1>
             <form>
-                <select onChange={handleSelect}>
-                    <option selected disabled>Select a user</option>
+                <select defaultValue={'DEFAULT'} onChange={handleSelect}>
+                    <option value={'DEFAULT'} disabled>Select a user</option>
                     {users.map(user =>
                         <option key={user.id} data-id={user.id}>{user.name}</option>)}
                 </select>
