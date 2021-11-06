636b5b5a0e7d6d67768fe1dfdcf835042f5daf2c
alexandrabaturina <alexandra.v.baturina@gmail.com>
2021-11-05 21:52:17 -0700

    feat: redirect to question details page

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,4 +1,6 @@
 import { useSelector } from "react-redux";
+import { Button } from 'react-bootstrap';
+import { useHistory } from "react-router";
 
 const Dashboard = ({ title, ids }) => {
 
@@ -12,17 +14,41 @@ const Dashboard = ({ title, ids }) => {
             optionTwo: state.questions[id].optionTwo.text,
             timestamp: state.questions[id].timestamp
         }))
-    )
+    );
+
+    let history = useHistory();
+
+    const handleClick = (question) => {
+        history.push(`/questions/${question}`);
+    }
 
     return (
         <div>
             <h1>{title}</h1>
-            <ul>
+            <div>
                 {questions
                     .sort((a, b) => b.timestamp - a.timestamp)
-                    .map(question =>
-                        <li key={question.id}>Would you rather {question.optionOne} or {question.optionTwo}?</li>)}
-            </ul>
+                    .map(question => (
+                        <div className='question-item' key={question.id}>
+
+                            <div className='question-info'>
+                                <p>
+                                    Would you rather <span className='bold'>{question.optionOne} </span>
+                                    or <span className='bold'>{question.optionTwo}</span>?
+                                </p>
+                                <span className='author-info'>Asked by {question.author}</span>
+                            </div>
+
+                            <Button
+                                variant="outline-dark"
+                                className='dashboard-button'
+                                onClick={() => handleClick(question.id)}>
+                                View
+                            </Button>
+                        </div>
+                    ))
+                }
+            </div>
         </div>
     )
 };
