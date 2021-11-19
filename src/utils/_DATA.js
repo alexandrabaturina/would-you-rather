bf9d7d98a0a5d1ede534c851e1481453981afd17
alexandrabaturina <alexandra.v.baturina@gmail.com>
2021-11-18 16:02:43 -0800

    chore: update mock data

--- src/utils/_DATA.js
+++ src/utils/_DATA.js
@@ -1,7 +1,7 @@
 let users = {
-    sarahedo: {
-        id: 'sarahedo',
-        name: 'Sarah Edo',
+    alexandrabaturina: {
+        id: 'alexandrabaturina',
+        name: 'Alexandra Baturina',
         avatarURL: 'https://svgcutsblog.com/blog/wp-content/uploads/2011/06/hummingbird-svg.png',
         answers: {
             "8xf0y6ziyjabvozdd253nd": 'optionOne',
@@ -11,9 +11,9 @@ let users = {
         },
         questions: ['8xf0y6ziyjabvozdd253nd', 'am8ehyc8byjqgar0jgpub9']
     },
-    tylermcginnis: {
-        id: 'tylermcginnis',
-        name: 'Tyler McGinnis',
+    lorenzoalpaca: {
+        id: 'lorenzoalpaca',
+        name: 'Lorenzo Alpaca',
         avatarURL: 'https://svgcutsblog.com/blog/wp-content/uploads/2011/06/winter-sheep-ear-muffs-svg-christmas.png',
         answers: {
             "vthrdm985a262al8qx3do": 'optionOne',
@@ -21,9 +21,9 @@ let users = {
         },
         questions: ['loxhs1bqm25b708cmbf3g', 'vthrdm985a262al8qx3do'],
     },
-    johndoe: {
-        id: 'johndoe',
-        name: 'John Doe',
+    brucemoose: {
+        id: 'brucemoose',
+        name: 'Bruce Moose',
         avatarURL: 'https://svgcutsblog.com/blog/wp-content/uploads/2011/06/fox-soccer-svg.png',
         answers: {
             "xj352vofupe1dqz9emx13r": 'optionOne',
@@ -37,10 +37,10 @@ let users = {
 let questions = {
     "8xf0y6ziyjabvozdd253nd": {
         id: '8xf0y6ziyjabvozdd253nd',
-        author: 'sarahedo',
+        author: 'alexandrabaturina',
         timestamp: 1467166872634,
         optionOne: {
-            votes: ['sarahedo'],
+            votes: ['alexandrabaturina'],
             text: 'have horrible short term memory',
         },
         optionTwo: {
@@ -50,66 +50,66 @@ let questions = {
     },
     "6ni6ok3ym7mf1p33lnez": {
         id: '6ni6ok3ym7mf1p33lnez',
-        author: 'johndoe',
+        author: 'brucemoose',
         timestamp: 1468479767190,
         optionOne: {
             votes: [],
             text: 'become a superhero',
         },
         optionTwo: {
-            votes: ['johndoe', 'sarahedo'],
+            votes: ['brucemoose', 'alexandrabaturina'],
             text: 'become a supervillain'
         }
     },
     "am8ehyc8byjqgar0jgpub9": {
         id: 'am8ehyc8byjqgar0jgpub9',
-        author: 'sarahedo',
+        author: 'alexandrabaturina',
         timestamp: 1488579767190,
         optionOne: {
             votes: [],
             text: 'be telekinetic',
         },
         optionTwo: {
-            votes: ['sarahedo'],
+            votes: ['alexandrabaturina'],
             text: 'be telepathic'
         }
     },
     "loxhs1bqm25b708cmbf3g": {
         id: 'loxhs1bqm25b708cmbf3g',
-        author: 'tylermcginnis',
+        author: 'lorenzoalpaca',
         timestamp: 1482579767190,
         optionOne: {
             votes: [],
             text: 'be a front-end developer',
         },
         optionTwo: {
-            votes: ['sarahedo'],
+            votes: ['alexandrabaturina'],
             text: 'be a back-end developer'
         }
     },
     "vthrdm985a262al8qx3do": {
         id: 'vthrdm985a262al8qx3do',
-        author: 'tylermcginnis',
+        author: 'lorenzoalpaca',
         timestamp: 1489579767190,
         optionOne: {
-            votes: ['tylermcginnis'],
+            votes: ['lorenzoalpaca'],
             text: 'find $50 yourself',
         },
         optionTwo: {
-            votes: ['johndoe'],
+            votes: ['brucemoose'],
             text: 'have your best friend find $500'
         }
     },
     "xj352vofupe1dqz9emx13r": {
         id: 'xj352vofupe1dqz9emx13r',
-        author: 'johndoe',
+        author: 'brucemoose',
         timestamp: 1493579767190,
         optionOne: {
-            votes: ['johndoe'],
+            votes: ['brucemoose'],
             text: 'write JavaScript',
         },
         optionTwo: {
-            votes: ['tylermcginnis'],
+            votes: ['lorenzoalpaca'],
             text: 'write Swift'
         }
     },
